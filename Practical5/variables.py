12c8e12837d335e6482dff6c547f2efdb41cc351
ZihanWANG-001 <79962850+ZihanWANG-001@users.noreply.github.com>
2021-03-09 21:30:39 +0800

    add practical5

---
+++ Practical5/variables.py
@@ -0,0 +1,18 @@
+#Some simple math
+a=110602  #My birthday
+b=190784  #Rob's birthday
+c=100321  #Today's date
+d=a-c
+e=b-a
+print('d=',d)
+print('e=',e)
+#Booleans
+x=True
+y=False
+z=(x and not y)or(y and not x)
+print(z)    #It is true if either x or y (but not both) are true. It is false if x and y are both true or both false.
+w=z!=y
+print(w)
+print(z==w) #To test if z and w are always the same.
+
+
