87c0a123d32a60a6801912ea9ae9c47036b89c18
ZihanWANG-001 <79962850+ZihanWANG-001@users.noreply.github.com>
2021-05-18 18:10:31 +0800

    Update GO.py

--- Practical14/GO.py
+++ Practical14/GO.py
@@ -8,66 +8,70 @@ collection=DOMTree.documentElement
 terms=collection.getElementsByTagName("term")
 #Preparing things needed for counting.
 #The fourth macromolecule I chose is glycoprotein.
-dna=[]
-rna=[]
-protein=[]
-glycoprotein=[]
-for term in terms:
-	defstr=term.getElementsByTagName("defstr")[0]#find the description element
-	text=defstr.childNodes[0].data #extract the description text
-	id=term.getElementsByTagName("id")[0] #find the id element
-	if re.findall(r'DNA',text):   #find whether the description contains dna/rna/protein/glycoprotein.
-		dna.append(id)
-	if re.findall(r'RNA',text):
-		rna.append(id)
-	if re.findall(r'protein',text):
-		protein.append(id)
-	if re.findall(r'Protein',text):
-		protein.append(id)
-	if re.findall(r'glycoprotein',text):
-		glycoprotein.append(id)	
-	if re.findall(r'Glycoprotein',text):
-		glycoprotein.append(id)
-		
-is_as=collection.getElementsByTagName("is_a")		#Collect all the <is_a> nodes
-def childNode_finder(x):
-	global count
-	global number
-	count=0
-	childNodeid=[]
-	for k in range(0,is_as.length):
-		for i in x:
-			if i.childNodes[0].data==is_as[k].childNodes[0].data:  #Extract the text and compare
-				count+=1
-				number+=1
-				childNodeid.append(is_as[k].parentNode.childNodes[1])
-	print(number)
-	if count!=0:
-		x=childNodeid
-		childNode_finder(x) #recursion, use the childnodes found as the new parent nodes.
-	else:                   #count=0 means there is no such id in <is_a>, so it has no child, it is the end of the node tree.
-		return number
-	
-		
-number=0
-childNode_finder(dna)
-number1=number
-print('childNodes_DNA',number1)
-number=0
-childNode_finder(rna)
-number2=number
-print('childNodes_RNA',number2)
-number=0
-childNode_finder(protein)
-number3=number
-print('childNodes_protein',number3)
-number=0
-childNode_finder(glycoprotein)
-number4=number
-print('childNodes_glycoprotein',number4)
 
+#Establish a dictionary. The key is the parent node and the corresponding value is the childnode of the key.
+#Store each id as one entry of the dictionary.
+#For each key create an empty list to store its childnodes.
+parent_child={} 
+for term in terms:
+	id=term.getElementsByTagName('id')[0].childNodes[0].data
+	parent_child[id]=[]
+#<is_a> is the parent nodes, they need to be stored in the key.
+for term in terms:
+	id1=term.getElementsByTagName('id')[0].childNodes[0].data #Get the child of the <is_a> and store it in the list of the <is_a> key.
+	is_as=term.getElementsByTagName('is_a')#An id can have more than one <is_a>. Use a loop to add them all into the list.
+	for is_a in is_as:
+		parent_child[is_a.childNodes[0].data].append(id1)
+#Now we have the dictionary that stored all one-layer relationship of parent-child.
+#There are some entry with an empty list. This means this GO:xxxx in the key has no childnode.
+#define the function to find all levels of childnodes of a given finder.
+
+#define a function to search for the given keyword.
+#The "finder" is either "DNA" or "RNA" or other keywords.
+#Search this "finder" in each <defstr>
+#If it is in the <defstr>, then find the <id> of this <defstr>. This is the parent node.
+#Search in the dictionary's key for this parent node.
+#If its list is not empty, this means it has childnode(s)
+#Make its list be the new "finder",find their childnodes.
+def search(finder):
+	for term in terms:
+		defstr=term.getElementsByTagName('defstr')[0].childNodes[0].data
+		if finder in defstr:
+			id2=term.getElementsByTagName('id')[0].childNodes[0].data
+			if parent_child[id2]!=[]:                 #The function will stop when there is no more childnodes for a given id-----reached the tip of the tree.
+				new_parent_list=parent_child[id2]
+				a=childNode_counter(new_parent_list)  #Call another function to repeatedly use the parent_child relationship built by the dictionary to find all the childnodes and store them in a list.
+	print('childNode number of',finder,':',a)         #a is the returned result of the other function, which is the length of the list which stores the childnodes.
+	global b                                          #because we need to store each "a" value to draw the pie chart, a global b is needed to make it accessible outside the function.
+	b=a
+	return a
+				
+children=[]		#Creat an empty list to store the childnodes.
+#Define another function to repeadly count the childnodes of previous nodes.		
+def childNode_counter(new_parent_list):
+	for j in range(0,len(new_parent_list)):#for each_previous_child in new_parent_list. 
+		if new_parent_list[j] not in children: #new_parent_list[j] is the previous child
+			children.append(new_parent_list[j]) #add the childnodes into the list
+			childNode_counter(parent_child[new_parent_list[j]]) #parent_child[new_parent_list[j]] is the child of the new parent (previous child)
+	return len(children)
+#Call the functions to search for DNA's childnodes
+search('DNA')
+a1=b
+#Call the functions to search for RNA's childnodes, and protein's and glycoprotein's
+#Remember to clear the childnode list each time.
+children=[]
+search('RNA')
+a2=b
+children=[]
+search('protein')
+a3=b
+children=[]
+search('glycoprotein')
+a4=b
+
+#Draw the pie chart
 labels='DNA','RNA','protein','glycoprotein'
-size=[number1,number2,number3,number4]
+size=[a1,a2,a3,a4]
 plt.label=('Number of childNodes')
 plt.pie(size,labels=labels,autopct='%1.1f%%',shadow=False,startangle=90)
 plt.axis('equal')
